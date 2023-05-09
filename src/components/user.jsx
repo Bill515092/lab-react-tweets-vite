f811faa3c1c043b56f807dc9aa772af346c315df
John Doe <johndoe@example.com>
2023-05-09 08:47:17 +0100

    handle '@' added

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -2,7 +2,7 @@ function User(props) {
   return (
     <span className="user">
       <span className="name">{props.userData.name} </span>
-      <span className="handle">{props.userData.handle} </span>
+      <span className="handle">@{props.userData.handle} </span>
     </span>
   );
 }
